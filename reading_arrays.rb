751e1b852229bcafb514cd05f6b5dd5bcffec432
Kathleen Eife <keife218@icloud.com>
2018-07-25 15:01:49 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -28,9 +28,6 @@ def last_student_by_method
 end
 
 def first_second_and_third_students
-  returnarray = []
-  returnarray[0]= STUDENT_NAMES[0]
-  returnarray[1] = STUDENT_NAMES[1]
-  returnarray[2] = STUDENT_NAMES[2]
+  returnarray = [STUDENT_NAMES[0], STUDENT_NAMES[1], STUDENT_NAMES[2]]
   return returnarray
 end
